9d7aed98c7fe7015443acd4df1e9021a71334a96
Matthew Courtney <matthewdc182@gmail.com>
2020-09-04 19:22:29 -0400

    added in the requirements ran init the intalled mysql

--- employee-tracker.js
+++ employee-tracker.js
@@ -13,3 +13,19 @@
 // 3 insert values for the table
 // 4 create db.js file 
 //     -console.table 
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
