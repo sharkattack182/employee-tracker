deb46e61c0ea3464d1499e476c4b1833daf0f253
Matthew Courtney <matthewdc182@gmail.com>
2020-09-04 18:55:01 -0400

    added in psudo code for step one

---
+++ employee-tracker.js
@@ -0,0 +1,15 @@
+
+
+// psudo code for step one
+
+// 1 set up database in sql 
+// 2 set up table
+//     -ID
+//     -first 
+//     -last 
+//     -dept 
+//     -salary 
+//     -manager (tied to the department) 
+// 3 insert values for the table
+// 4 create db.js file 
+//     -console.table 
