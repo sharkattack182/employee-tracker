e12504bf88c7aedff2f62169a35b36505d84422d
Matthew Courtney <matthewdc182@gmail.com>
2020-09-05 00:47:57 -0400

    added the inquirer prompt and returned the employee table added in notes as psudo code to know what each conditional needs to do1'

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,21 +1,38 @@
 
 
-// psudo code for step one
-
-// 1 set up database in sql 
-// 2 set up table
-//     -ID
-//     -first 
-//     -last 
-//     -dept 
-//     -salary 
-//     -manager (tied to the department) 
-// 3 insert values for the table
-// 4 create db.js file 
-//     -console.table 
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+
+
+
+
+inquirer.prompt([
+    {
+        type: "list",
+        message: "What do you want to do?",
+        choices: ["View Employees", "View Departments", "View Roles", "Add Employee", "Add Department", "Add Role", "Update Employee Role"],
+        name: "action"
+    },
+])
+.then(answers => {
+    if(answers.action === "View Employees") {
+       readEmployees(); //read employees
+    }else  if(answers.action === "View Departments") {
+        readEmployees(); // read departments
+    }else  if(answers.action === "View Roles") {
+        readEmployees(); // read roles
+    }else  if(answers.action === "Add Employee") {
+        readEmployees(); // need to add more prompt to get the info first name last name role id and manager id 
+    }else  if(answers.action === "Add Role") {
+        readEmployees(); //need to add prompts title salary and dept id
+    }else  if(answers.action === "Add Department") {
+        readEmployees(); // add prompy for dept name 
+    }else  if(answers.action === "Update Employee Role") {
+        readEmployees(); // need to target employee then update profile 
+    }
+})
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -30,63 +47,64 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     //  newEmployee();
     // updateEmployee();
-     deleteEmployee();
+    //  deleteEmployee();
+    // readEmployees();
 });
 
-function newEmployee() { //this works added matt successfully
-    console.log("Creating new employee profile...\n");
-    var query = connection.query(
-        "INSERT INTO employees SET ?",
-        {
-            first_name: "Matt",
-            last_name: "Courtney",
-            department: "IT",
-            salary: 50000,
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " employee inserted!\n");
-            // Call updateProduct AFTER the INSERT completes
-            //updateEmployee();
-            readEmployees();
-        }
-    );
-}   
-    function updateEmployee() {
-        console.log("Updating Matt's profile...\n");
-        var query = connection.query(
-            "UPDATE employees SET ? WHERE ?",
-            [
-                {
-                    department: "HR"
-                },
-                {
-                    first_name: "Matt"
-                }
-            ],
-            function (err, res) {
-                if (err) throw err;
-                console.log(res.affectedRows + " products updated!\n");
-                readEmployees();
-            }
-        );  
-    }
+// function newEmployee() { //this works added matt successfully
+//     console.log("Creating new employee profile...\n");
+//     var query = connection.query(
+//         "INSERT INTO employees SET ?",
+//         {
+//             first_name: "Matt",
+//             last_name: "Courtney",
+//             department: "IT",
+//             salary: 50000,
+//         },
+//         function (err, res) {
+//             if (err) throw err;
+//             console.log(res.affectedRows + " employee inserted!\n");
+//             // Call updateProduct AFTER the INSERT completes
+//             //updateEmployee();
+//             readEmployees();
+//         }
+//     );
+// }   
+//     function updateEmployee() {
+//         console.log("Updating Matt's profile...\n");
+//         var query = connection.query(
+//             "UPDATE employees SET ? WHERE ?",
+//             [
+//                 {
+//                     department: "HR"
+//                 },
+//                 {
+//                     first_name: "Matt"
+//                 }
+//             ],
+//             function (err, res) {
+//                 if (err) throw err;
+//                 console.log(res.affectedRows + " products updated!\n");
+//                 readEmployees();
+//             }
+//         );  
+//     }
 
-  function deleteEmployee() {
-    console.log("Deleting employee...\n");
-    var query = connection.query(
-      "DELETE FROM employees WHERE ?",
-      {
-        first_name: "Matt"
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " employee deleted!\n");
-        // Call readProducts AFTER the DELETE completes
-        readEmployees();
-      }
-    );
-  }
+//   function deleteEmployee() {
+//     console.log("Deleting employee...\n");
+//     var query = connection.query(
+//       "DELETE FROM employees WHERE ?",
+//       {
+//         first_name: "Matt"
+//       },
+//       function(err, res) {
+//         if (err) throw err;
+//         console.log(res.affectedRows + " employee deleted!\n");
+//         // Call readProducts AFTER the DELETE completes
+//         readEmployees();
+//       }
+//     );
+//   }
 
 function readEmployees() {
     connection.query("SELECT * FROM employees", function (err, res) {
