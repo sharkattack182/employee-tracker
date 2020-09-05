973ca4ebf306267a5a0bed99fbfcdb0eadffeea8
Matthew Courtney <matthewdc182@gmail.com>
2020-09-04 21:19:12 -0400

    added in the functions to add udate and delete employees

--- employee-tracker.js
+++ employee-tracker.js
@@ -17,23 +17,80 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "employee_tracker_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "employee_tracker_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    //  newEmployee();
+    // updateEmployee();
+     deleteEmployee();
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM employees", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
+function newEmployee() { //this works added matt successfully
+    console.log("Creating new employee profile...\n");
+    var query = connection.query(
+        "INSERT INTO employees SET ?",
+        {
+            first_name: "Matt",
+            last_name: "Courtney",
+            department: "IT",
+            salary: 50000,
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " employee inserted!\n");
+            // Call updateProduct AFTER the INSERT completes
+            //updateEmployee();
+            readEmployees();
+        }
+    );
+}   
+    function updateEmployee() {
+        console.log("Updating Matt's profile...\n");
+        var query = connection.query(
+            "UPDATE employees SET ? WHERE ?",
+            [
+                {
+                    department: "HR"
+                },
+                {
+                    first_name: "Matt"
+                }
+            ],
+            function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows + " products updated!\n");
+                readEmployees();
+            }
+        );  
+    }
+
+  function deleteEmployee() {
+    console.log("Deleting employee...\n");
+    var query = connection.query(
+      "DELETE FROM employees WHERE ?",
+      {
+        first_name: "Matt"
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " employee deleted!\n");
+        // Call readProducts AFTER the DELETE completes
+        readEmployees();
+      }
+    );
+  }
+
+function readEmployees() {
+    connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
     });
-  };
+};
